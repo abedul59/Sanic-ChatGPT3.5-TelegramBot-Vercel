8c1824792d5129e1c92ba8b81a2962d0105e3311
abedul59 <70437442+abedul59@users.noreply.github.com>
2023-04-09 20:23:51 +0800

    Update index.py

--- src/index.py
+++ src/index.py
@@ -1,92 +1,63 @@
-from sanic import Sanic
+from sanic import Sanic, Request
 from sanic import response
-from sanic.request import Request
 from sanic.response import HTTPResponse
-import httpx
+from sanic.request import Request
+from sanic import response
 
 import re
 import json
-
+import httpx
+import os
+import openai
 
 TOKEN = str(os.getenv("TELEGRAM_BOT_TOKEN"))
 BASE_URL = f"https://api.telegram.org/bot{TOKEN}"
 
 client = httpx.AsyncClient()
 
-
 app = Sanic(__name__)
-################################################################
-import openai, os
-	
+
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-
-
-	
 conversation = []
 
-class ChatGPT:  
-    
-
+class ChatGPT:
     def __init__(self):
-        
         self.messages = conversation
-        self.model = os.getenv("OPENAI_MODEL", default = "gpt-3.5-turbo")
-
-
+        self.model = os.getenv("OPENAI_MODEL", default="gpt-3.5-turbo")
 
     def get_response(self, user_input):
         conversation.append({"role": "user", "content": user_input})
         
-
-        response = openai.ChatCompletion.create(
-	            model=self.model,
-                messages = self.messages
-
-                )
-
-        conversation.append({"role": "assistant", "content": response['choices'][0]['message']['content']})
+        response = openai.Completion.create(
+            engine=self.model,
+            prompt=f"{conversation[-1]['role']}: {conversation[-1]['content']}\nAI:",
+            max_tokens=1024,
+            n=1,
+            stop=None,
+            temperature=0.5,
+        )
+        
+        message = response.choices[0].text.strip()
+        conversation.append({"role": "assistant", "content": message})
         
         print("AI回答內容：")        
-        print(response['choices'][0]['message']['content'].strip())
+        print(message)
 
-
-        
-        return response['choices'][0]['message']['content'].strip()
-	
-
-
+        return message
 
 chatgpt = ChatGPT()
 
-
-#from sanic import Sanic
-#from sanic.response import json, HTTPException
-
- 
- 
-@app.route('/')
+@app.route("/")
 async def handle_request(request: Request) -> HTTPResponse:
     return response.text("Hello!")
-
 
 @app.post("/callback")
 async def callback(request: Request) -> HTTPResponse:
     data = await request.json()
-    chat_id = data['message']['chat']['id']
-    text = data['message']['text']
-    ai_reply_response = chatgpt.get_response(text)  
-
+    chat_id = data["message"]["chat"]["id"]
+    text = data["message"]["text"]
+    ai_reply_response = chatgpt.get_response(text)
     await client.get(f"{BASE_URL}/sendMessage?chat_id={chat_id}&text={ai_reply_response}")
-
-    return data
-
-
-
     return response.text("OK")
 
-
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8006)
- 
- 
