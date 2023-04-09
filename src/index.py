950aa6465dbc84d894fa02201ce149fe56fd9ac5
abedul59 <70437442+abedul59@users.noreply.github.com>
2023-04-09 20:39:13 +0800

    Update index.py

--- src/index.py
+++ src/index.py
@@ -50,13 +50,22 @@ chatgpt = ChatGPT()
 @app.route("/")
 async def handle_request(request: Request) -> HTTPResponse:
     return response.text("Hello!")
-
+'''
 @app.post("/callback")
 async def callback(request: Request) -> HTTPResponse:
     data = await request.json()
     chat_id = data["message"]["chat"]["id"]
     text = data["message"]["text"]
     ai_reply_response = chatgpt.get_response(text)
+    await client.get(f"{BASE_URL}/sendMessage?chat_id={chat_id}&text={ai_reply_response}")
+    return response.text("OK")
+'''
+@app.post("/callback")
+async def callback(request: Request) -> HTTPResponse:
+    req_data = await request.json()
+    chat_id = req_data['message']['chat']['id']
+    text = req_data['message']['text']
+    ai_reply_response = chatgpt.get_response(text)  
     await client.get(f"{BASE_URL}/sendMessage?chat_id={chat_id}&text={ai_reply_response}")
     return response.text("OK")
 
