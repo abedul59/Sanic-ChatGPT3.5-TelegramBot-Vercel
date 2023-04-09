4eead1137f5a9f34eae4bdd120141573903a2f33
abedul59 <70437442+abedul59@users.noreply.github.com>
2023-04-09 20:51:31 +0800

    Update index.py

--- src/index.py
+++ src/index.py
@@ -20,32 +20,37 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 conversation = []
 
-class ChatGPT:
+class ChatGPT:  
+    
+
     def __init__(self):
+        
         self.messages = conversation
-        self.model = os.getenv("OPENAI_MODEL", default="gpt-3.5-turbo")
+        self.model = os.getenv("OPENAI_MODEL", default = "gpt-3.5-turbo")
+
+
 
     def get_response(self, user_input):
         conversation.append({"role": "user", "content": user_input})
         
-        response = openai.Completion.create(
-            engine=self.model,
-            prompt=f"{conversation[-1]['role']}: {conversation[-1]['content']}\nAI:",
-            max_tokens=1024,
-            n=1,
-            stop=None,
-            temperature=0.5,
-        )
-        
-        message = response.choices[0].text.strip()
-        conversation.append({"role": "assistant", "content": message})
+
+        response = openai.ChatCompletion.create(
+	            model=self.model,
+                messages = self.messages
+
+                )
+
+        conversation.append({"role": "assistant", "content": response['choices'][0]['message']['content']})
         
         print("AI回答內容：")        
-        print(message)
+        print(response['choices'][0]['message']['content'].strip())
 
-        return message
+
+        
+        return response['choices'][0]['message']['content'].strip()
 
 chatgpt = ChatGPT()
+
 
 @app.route("/")
 async def handle_request(request: Request) -> HTTPResponse:
